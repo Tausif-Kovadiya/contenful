3a07d547f0fbeee4ae636aa617ad2ed93af47028
Tausif-Kovadiya <tausif.emaad@gmail.com>
2025-02-08 13:38:43 +0530

    blog change

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,7 +1,8 @@
 import { client } from "../contentful/Client";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import Card from "./components/Card";
+// import Card from "./components/Card";
 export const runtime = "edge"
+import Image from "next/image";
 
 
 // interface Item {
@@ -18,29 +19,41 @@ const Blogs = async () => {
     content_type: "blogs",
   });
 
-  // console.log(items)
+  console.log(items);
 
-return (
-  <>
-    <h1>Blogs Page</h1>
-    <br />
-    <div className="container">
-      <div className="row">
-        {items.map((item) => (
-          <div key={item.sys.id} className="col-md-4 mb-4">
-            <Card
+  return (
+    <>
+      <h1>Blogs Page</h1>
+      <br />
+      <div className="container">
+        <div className="row gy-2">
+          {items.map((item) => (
+            <div key={item.sys.id} className="col-md-4">
+              <div className="bg-light">
+              <h4>{item?.fields?.blogTitle}</h4>
+              <Image
+                alt={item?.fields?.blogTitle}
+                width={250}
+                height={250}
+                src={`https:${item?.fields?.blogImage?.fields?.file?.url}`}
+              />
+              <div style={{ margin: 10 }}>
+                {documentToReactComponents(item?.fields?.blogDescription)}
+              </div>
+              </div>
+              {/* <Card
               id={item.sys.id}
               title={item?.fields?.blogTitle}
               text={documentToReactComponents(item?.fields?.blogDescription)}
               imgSrc={`https:${item?.fields?.blogImage?.fields?.file?.url}`}
               slug={item?.fields?.slug}
-            />
-          </div>
-        ))}
+            /> */}
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
 };
 
 export default Blogs;
